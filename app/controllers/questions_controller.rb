576fd03b88d3f5bed1046b1cd8b0d9a3b1edb3ce
phbasin <pierre-henri.basin@orange.fr>
2020-05-11 15:02:13 +0200

    02-finished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
-    @question = params[:answer]
   end
+
   def answer
+    @question = params[:answer]
+    if @question == 'I am going to work'
+      @response = 'Great!'
+    elsif @question[-1] == '?'
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
